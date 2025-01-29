5a870e4290bc5e92ad18c8832479f6697c7e9a82
mschmotzer <mschmotzer@student.ethz.ch>
2025-01-29 19:50:41 +0100

    External Calibration Works

---
+++ calibration_zed.py
@@ -0,0 +1,114 @@
+import cv2
+import numpy as np
+import argparse
+import yaml 
+
+import pyzed.sl as sl
+
+from zed_util import init_zed
+
+chessboard_size = (11, 8)  # Change this to your chessboard's size
+square_size = 30  # Define the real-world size of a square in mm
+
+def main(args):
+    zed_list = init_zed(args.calib_svo_path)
+    zed_serials = [zed.get_camera_information().serial_number for zed in zed_list]
+    intrinsics = [zed.get_camera_information().camera_configuration.calibration_parameters.left_cam for zed in zed_list]
+    intrinsics = [{
+    'fx': intr.fx, 
+    'fy': intr.fy,
+    'cx': intr.cx,
+    'cy': intr.cy,
+    } for intr in intrinsics]
+    
+    disto = [zed.get_camera_information().camera_configuration.calibration_parameters.left_cam.disto for zed in zed_list]
+    
+    # create intrinsics matrix
+    intrinsics_mat = []
+    for intr in intrinsics:
+        fx, fy, cx, cy = intr['fx'], intr['fy'], intr['cx'], intr['cy']
+        intrinsics_mat.append(np.array([[fx, 0, cx], [0, fy, cy], [0, 0, 1]], dtype=np.float32))
+
+
+    images = [sl.Mat() for _ in zed_list]
+    images_np = []
+    runtime = sl.RuntimeParameters()
+    for idx, zed in enumerate(zed_list):
+        err = zed.grab(runtime)
+        if err != sl.ERROR_CODE.SUCCESS:
+            print(f"Error grabbing images from ZED camera {zed_serials[idx]}")
+            exit()
+        
+        zed.retrieve_image(images[idx], sl.VIEW.LEFT)
+        image_np = images[idx].get_data()[:, :, :-1]  ## leave out alpha
+        images_np.append(image_np)
+    
+
+    for cam_idx, image in enumerate(images_np):
+        # must be reset at each iteration to avoid error
+        objp = np.zeros((np.prod(chessboard_size), 3), np.float32)
+        objp[:, :2] = np.indices(chessboard_size).T.reshape(-1, 2)
+        objp *= square_size
+
+        objpoints = []  # 3d points in real-world space
+        imgpoints = []  # 2d points in image plane.  
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)  
+        """cv2.imshow("Image", gray)
+        cv2.waitKey()
+        cv2.destroyAllWindows()"""
+        ret, corners = cv2.findChessboardCorners(gray, chessboard_size, None)
+        if ret:
+            objpoints.append(objp)
+            # Refine the corner positions
+            criteria = (cv2.TermCriteria_EPS + cv2.TermCriteria_MAX_ITER, 30, 0.001)
+            corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
+            imgpoints.append(corners2)
+        else:
+            print("Chessboard corners not found.")
+            exit()
+
+        """ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], intrinsics_mat[cam_idx], disto[cam_idx],
+                                                           flags=cv2.CALIB_USE_INTRINSIC_GUESS+
+                                                                cv2.CALIB_FIX_INTRINSIC +
+                                                                cv2.CALIB_FIX_PRINCIPAL_POINT+cv2.CALIB_FIX_FOCAL_LENGTH)"""
+        ret, rvec, tvec = cv2.solvePnP(objpoints[0], imgpoints[0], intrinsics_mat[cam_idx], disto[cam_idx], None, None, useExtrinsicGuess=False, flags=cv2.SOLVEPNP_ITERATIVE)
+        if ret:            
+            rotation_matrix, _ = cv2.Rodrigues(rvec)
+            extr_4x4 = np.eye(4)
+            extr_4x4[:3, :3] = rotation_matrix
+            extr_4x4[:3, 3] = tvec.T
+            
+            # Visualize the calibration results
+            imgpoints2, _ = cv2.projectPoints(objpoints[0], rvec, tvec, intrinsics_mat[cam_idx], disto[cam_idx])
+            error = cv2.norm(imgpoints[0], imgpoints2, cv2.NORM_L2) / len(imgpoints2)
+            print(f"Total reprojection error: {error}")
+
+            objpoints[0] += np.array([1, 1, 0]) * square_size
+            points_proj, _ = cv2.projectPoints(objpoints[0], rvec, tvec, intrinsics_mat[cam_idx], disto[cam_idx])
+            print("points_proj succeeded")
+
+            # create cv2 mat from np array
+            img = np.ascontiguousarray(image, dtype=np.uint8)
+            cv2.drawChessboardCorners(img, chessboard_size, points_proj, True)
+            cv2.drawFrameAxes(img, intrinsics_mat[cam_idx], disto[cam_idx], rvec, tvec, 300)
+            cv2.imshow("Projected points", img)
+            cv2.waitKey()
+            cv2.destroyAllWindows()
+            
+            with open(rf"{args.calib_output_path}/extr_{cam_idx}_{zed_serials[cam_idx]}.yml", 'w') as file:
+                yaml.dump({'extr_4x4': extr_4x4.tolist()}, file)
+
+            print("Translation vector:\n", tvec)
+        else:
+            print("Camera calibration failed.")
+    
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Args for camera calibration")
+    # Must calibrate for each m -(machine) folder separately
+    parser.add_argument('--calib_svo_path', type=str, default=None, help="Path to folder with calibration SVO files.")
+    parser.add_argument('--calib_output_path', type=str, default=None, help="Output folder for calibration files.")
+
+    args = parser.parse_args()
+    
+    main(args)
---
+++ zed_util.py
@@ -0,0 +1,31 @@
+import pyzed.sl as sl
+
+def init_zed(calib_path):
+    init = sl.InitParameters()
+    init.camera_resolution = sl.RESOLUTION.HD720
+    init.camera_fps = 30  # The framerate is lowered to avoid any USB3 bandwidth issues
+    init.depth_mode = sl.DEPTH_MODE.NEURAL
+    init.coordinate_units = sl.UNIT.MILLIMETER
+
+
+    #List and open cameras
+    zed_list = []
+    name_list = []
+    last_ts_list = []
+    cameras = sl.Camera.get_device_list()
+    index = 0
+    for cam in cameras:
+        init.set_from_serial_number(cam.serial_number)
+        name_list.append("ZED {}".format(cam.serial_number))
+        print("Opening {}".format(name_list[index]))
+        cam = sl.Camera()
+        _ = cam.open(init)
+        zed_list.append(cam)
+    
+    return zed_list
+
+if __name__ == "__main__":
+    zed = init_zed("calibration.svo")
+    runtime = sl.RuntimeParameters()
+    image = sl.Mat()
+    
